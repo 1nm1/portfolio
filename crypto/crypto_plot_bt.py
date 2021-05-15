2d0594927bbfb7a809ab930b23e0c1a51a64cb89
1nm1 <n.k.meek@gmail.com>
2021-05-15 12:10:51 -0700

    cleaning up code for main script, adding example notebook

--- crypto/crypto_plot_bt.py
+++ crypto/crypto_plot_bt.py
@@ -1,4 +1,4 @@
-#region Import Libraries
+# region Import Libraries
 from fastquant import get_crypto_data
 from fastquant import backtest
 import pandas as pd
@@ -9,9 +9,10 @@ import json
 import os
 import sys
 import general_script_functions as gsf
-#endregion 
+# endregion
 
-def data_api(pair, start_date,end_date, time_resolution):
+
+def data_api(pair, start_date, end_date, time_resolution):
     '''
     Summary:
     ----------
@@ -30,7 +31,7 @@ def data_api(pair, start_date,end_date, time_resolution):
 
     Outputs:
     ----------
-    crypto : dataframe 
+    crypto : dataframe
         financial data in OHLCV form
     '''
     crypto = get_crypto_data(
@@ -38,17 +39,18 @@ def data_api(pair, start_date,end_date, time_resolution):
         start_date,
         end_date,
         time_resolution=time_resolution)
-    
+
     return crypto
 
+
 @gsf.func_perform_time
-def fetch_data(pair, start_date,end_date, time_resolution):
+def fetch_data(pair, start_date, end_date, time_resolution):
     '''
     Summary:
     ----------
     Manages the data retrievel from Binance API.
     Due to API limitation, this function continues to collect data
-    if the required dataset is larger than 500 data points (does this by 
+    if the required dataset is larger than 500 data points (does this by
     checking if end date equals the requested end date)
 
     Params:
@@ -64,27 +66,27 @@ def fetch_data(pair, start_date,end_date, time_resolution):
 
     Outputs:
     ----------
-    crypto : dataframe 
+    crypto : dataframe
         financial data in OHLCV form
     '''
     print("\n=== Fetching Data ===")
     print(f"\tAsset / Pair: {pair}\n\tStart Date: {start_date}\n\tEnd Date: {end_date}\n\tResolution: {time_resolution}")
 
     # Retrieve initial set of data
-    df_main = data_api(pair, start_date,end_date, time_resolution)
+    df_main = data_api(pair, start_date, end_date, time_resolution)
 
     # Check if end date matches requested end date
     end_date_date = dt.datetime.strptime(end_date, '%Y-%m-%d').date()
     df_end_date = df_main.iloc[[-1]].index.date[0]
-    
+
     # If dates don't match, collect additional data
     if end_date_date != df_end_date:
         data_trigger = True
-        while data_trigger == True:        
+        while data_trigger == True:
             df = data_api(
-                pair, 
+                pair,
                 df_end_date.strftime('%Y-%m-%d'),
-                end_date, 
+                end_date,
                 time_resolution)
             df_end_date = df.iloc[[-1]].index.date[0]
             df_main = pd.concat([df_main, df])
@@ -92,9 +94,10 @@ def fetch_data(pair, start_date,end_date, time_resolution):
             if int((end_date_date-df_end_date).days) < 1:
                 data_trigger = False
 
-    print(f"\n=== Data Successfully Retrieved ===")
-    
+    print("\n=== Data Successfully Retrieved ===")
+
     return df_main
+
 
 @gsf.func_perform_time
 def df_trans_calcs(df):
@@ -102,7 +105,7 @@ def df_trans_calcs(df):
     Summary:
     ----------
     Performs transformations and calculations on dataframe
-    
+
     Params:
     ----------
     df : pandas dataframe
@@ -115,15 +118,16 @@ def df_trans_calcs(df):
     '''
     periods = 10
     df['close pert change'] = df['close'].pct_change(periods=periods) * 100
-    df['close delta'] = df['close'].diff(periods=periods) 
+    df['close delta'] = df['close'].diff(periods=periods)
     df['vol pert change'] = df['volume'].pct_change(periods=periods) * 100
     df['vol delta'] = df['volume'].diff(periods=periods)
     df['close pert change ewm'] = df['close pert change'].ewm(span=20).mean()
     df['vol pert change ewm'] = df['vol pert change'].ewm(span=20).mean()
     return df
 
+
 @gsf.func_perform_time
-def plot_data(df, pair, time_resolution):
+def plot_data(df, pair, start_date, end_date, time_resolution):
     '''
     Summary:
     ----------
@@ -131,10 +135,14 @@ def plot_data(df, pair, time_resolution):
 
     Params:
     ----------
-    df : dataframe 
+    df : dataframe
         financial data in OHLCV form
     pair : str
         crypto pair to retrieve (i.e. BTC/USDT)
+    start_date : str
+        starting date in dataset (YYYY-MM-DD)
+    end_date : str
+        ending date in dataset (YYYY-MM-DD)
     time_resolution : str
         resolution in data set (1h, 1d, 1w)
 
@@ -142,56 +150,64 @@ def plot_data(df, pair, time_resolution):
     ----------
     none
     '''
+    # Create figure object
     fig = make_subplots(
             rows=1,
             cols=2,
-            specs=[[{"secondary_y": True},{"secondary_y": True}]])
+            specs=[[{"secondary_y": True}, {"secondary_y": True}]])
 
+    # Create dictionary with figure params
     fig_dict = {
         'fig1':
             {
-                'name':'Volume','x':df.index,'y':df['volume'],
-                'mode':'lines','opacity':0.75,'line':dict(color='darkgreen', width=1),
-                'secondary_y':True,'row':1,'col':1
+                'name': 'Volume', 'x': df.index, 'y': df['volume'],
+                'mode': 'lines', 'opacity': 0.75,
+                'line': dict(color='darkgreen', width=1),
+                'secondary_y': True, 'row': 1, 'col': 1
             },
         'fig2':
             {
-                'name':'Close','x':df.index,'y':df['close'],
-                'mode':'lines','opacity':1.0,'line':dict(color='darkblue', width=2),
-                'secondary_y':False,'row':1,'col':1
-            }, 
+                'name': 'Close', 'x': df.index, 'y': df['close'],
+                'mode': 'lines', 'opacity': 1.0,
+                'line': dict(color='darkblue', width=2),
+                'secondary_y': False, 'row': 1, 'col': 1
+            },
         'fig3':
             {
-                'name':'Volume % Change EWM','x':df.index,'y':df['vol pert change ewm'],
-                'mode':'lines','opacity':0.75,'line':dict(color='darkgreen', width=1),
-                'secondary_y':True,'row':1,'col':2
-            }, 
+                'name': 'Volume % Change EWM', 'x': df.index,
+                'y': df['vol pert change ewm'], 'mode': 'lines',
+                'opacity': 0.75, 'line': dict(color='darkgreen', width=1),
+                'secondary_y': True, 'row': 1, 'col': 2
+            },
         'fig4':
             {
-                'name':'Close % Change EWM','x':df.index,'y':df['close pert change ewm'],
-                'mode':'lines','opacity':1.0,'line':dict(color='darkblue', width=2),
-                'secondary_y':False,'row':1,'col':2
-            },  
+                'name': 'Close % Change EWM', 'x': df.index,
+                'y': df['close pert change ewm'], 'mode': 'lines',
+                'opacity': 1.0, 'line': dict(color='darkblue', width=2),
+                'secondary_y': False, 'row': 1, 'col': 2
+            },
         }
-    
+
+    # Create figures for each figure in fig_dict
     for fig_ in fig_dict:
         figure = fig_dict[fig_]
         fig.add_trace(
             go.Scattergl(
                 name=figure['name'],
-                x = figure['x'],
-                y = figure['y'],
+                x=figure['x'],
+                y=figure['y'],
                 mode=figure['mode'],
                 opacity=figure['opacity'],
                 line=figure['line']
                 ),
-                secondary_y=figure['secondary_y'],
-                row=figure['row'],
-                col=figure['col']
+            secondary_y=figure['secondary_y'],
+            row=figure['row'],
+            col=figure['col']
             )
-    
+
     fig.update_layout(title_text=f"{pair}, {time_resolution}  |  {start_date} --> {end_date}")
     fig.show()
+
 
 @gsf.func_perform_time
 def backtest_data_gdsr(df):
@@ -199,7 +215,7 @@ def backtest_data_gdsr(df):
     Summary:
     ----------
     Open json trading strategy file, test strategies through fastquant backtest
-    
+
     Params:
     ----------
     df : dataframe with financial data
@@ -212,13 +228,13 @@ def backtest_data_gdsr(df):
     json_file_path = os.path.join(sys.path[0], 'bt_strats_params.json')
 
     with open(json_file_path, encoding='utf-8', errors='ignore') as json_data:
-     strat_dict = json.load(json_data, strict=False)
-    
+        strat_dict = json.load(json_data, strict=False)
+
     for strat in strat_dict['strats']:
         bt = backtest(
             "multi",
             df,
-            strats=strat, 
+            strats=strat,
             plot=True)
         print(bt)
 
@@ -230,7 +246,7 @@ if __name__ == "__main__":
     pair = "ETH/USDT"
     start_date = "2021-03-10"
     end_date = "2021-05-15"
-    time_resolution = "15m" #15m 30m 1h 1d 1w
+    time_resolution = "15m"  # 15m 30m 1h 1d 1w
 
     # Retrieve financial data
     df = fetch_data(pair, start_date, end_date, time_resolution)
@@ -239,12 +255,9 @@ if __name__ == "__main__":
     df = df_trans_calcs(df)
 
     # Plot data
-    plot_data(df, pair, time_resolution)
-    
+    plot_data(df, pair, start_date, end_date, time_resolution)
+
     # Test trading strategies
     test_strat = False
-    if test_strat == True:
+    if test_strat is True:
         backtest_data_gdsr(df)
-        
-    
-    
