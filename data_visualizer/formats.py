682cdac7d0379215c2d435a760118c304c5dde9e
1nm1 <n.k.meek@gmail.com>
2021-05-30 12:06:08 -0700

    cleaning up code

--- data_visualizer/formats.py
+++ data_visualizer/formats.py
@@ -1,70 +1,69 @@
 # ==============
 # region Main application formats
-sidebar_style_1={
+sidebar_style_1 = {
     "position": "fixed",
     "top": 0,
     "left": 0,
     "bottom": 0,
     "width": "300px",
     "padding": "2rem 1rem",
-    "background-color": "#212121",}
+    "background-color": "#212121"}
 
 content_style_1 = {
     "margin-left": "18rem",
     "margin-right": "2rem",
-    "padding": "1rem 1rem",}
-# endregion 
+    "padding": "1rem 1rem"}
+# endregion
 # ==============
 
 
 # ==============
 # region Data Viz Page formats
 
+nav_bar_style_1 = {
+    'height': '70px',
+    'margin': '10px'}
 
-nav_bar_style_1={'height': '70px', 'margin':'10px'}
-
-
-
-# endregion 
+# endregion
 # ==============
 
 
 # ==============
 # region Database settings formats
 formgroup_settings_style_1 = {
-    'max-width':'700px',
-    'padding':'15px 15px 0px 15px'
+    'max-width': '700px',
+    'padding': '15px 15px 0px 15px'
 }
 
 formgroup_settings_input_style_1 = {
-    "width":"400px"
+    "width": "400px"
 }
 
 formgroup_settings_label_style_1 = {
-    "width":"175px",
-    "padding":"5px",
-    "text-align":"right",
-    "font-size":"18px",
-    "margin":"0px 10px 0px 0px"
+    "width": "175px",
+    "padding": "5px",
+    "text-align": "right",
+    "font-size": "18px",
+    "margin": "0px 10px 0px 0px"
 }
 
 alert_settings_style_1 = {
-    "width":"150px",
-    "display":"inline-block",
-    "margin":"10px",
-    "text-align":"center"
+    "width": "150px",
+    "display": "inline-block",
+    "margin": "10px",
+    "text-align": "center"
 }
 
 button_settings_connect_style_1 = {
-    "width":"125px",
-    "display":"inline-block",
-    "margin":"10px",
+    "width": "125px",
+    "display": "inline-block",
+    "margin": "10px",
 }
 
 button_settings_clear_form_style_1 = {
-    "width":"125px",
-    "display":"inline-block",
-    "margin":"10px 0px 10px 325px",
+    "width": "125px",
+    "display": "inline-block",
+    "margin": "10px 0px 10px 325px",
 }
-# endregion 
+# endregion
 # ==============
