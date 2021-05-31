6d0c0d187d2b10f5bfa71259cc2770035edbda3f
1nm1 <n.k.meek@gmail.com>
2021-05-30 17:18:45 -0700

    added connection to db functionality. Added testing params

--- data_visualizer/sql_data_viz_functions.py
+++ data_visualizer/sql_data_viz_functions.py
@@ -1,11 +1,32 @@
 import sqlalchemy as sa
 
 
-def connect_to_db(server, database, user, password):
-    from sqlalchemy import create_engine
+def connect_to_db(server='localhost',
+                  database='data_viz_demo',
+                  user='data_viz_user',
+                  password='data_viz_pw_1!'):
+    if server is None:
+        server='localhost'
+        database='data_viz_demo'
+        user='data_viz_user'
+        password='data_viz_pw_1!'
 
-    db_string = f"postgres://{user}:{password}@{server}:15813/{database}"
+    db_string = f"postgres://{user}:{password}@{server}:5432/{database}"
+    print(f"\t[MSG] Connecting to DB\n\t{db_string}")
 
-    engine = sa.create_engine(db_string)
+    table_q = """
+                SELECT table_name
+                FROM information_schema.tables
+                WHERE table_schema = 'public'
+                ORDER BY table_name;
+                """
+    try:
+        engine = sa.create_engine(db_string)
+        tables = engine.execute(table_q).fetchall()
+        available_tables = [table[0] for table in tables]
+        return [available_tables, 'success', None]
 
-    return engine
+    except Exception as ex:
+        print(ex.args)
+        return [None, 'failure', ex.args]
+
