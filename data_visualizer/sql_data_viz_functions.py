cf0ed979e9a504d615175fd05d0ee76516ddfc00
1nm1 <n.k.meek@gmail.com>
2021-05-30 19:04:34 -0700

    added extract_columns function

--- data_visualizer/sql_data_viz_functions.py
+++ data_visualizer/sql_data_viz_functions.py
@@ -1,18 +1,23 @@
 import sqlalchemy as sa
+from sqlalchemy.sql import text
 
 
-def connect_to_db(server='localhost',
-                  database='data_viz_demo',
-                  user='data_viz_user',
-                  password='data_viz_pw_1!'):
-    if server is None:
-        server='localhost'
-        database='data_viz_demo'
-        user='data_viz_user'
-        password='data_viz_pw_1!'
+def connect_to_db(server, database, user, password):
+    global engine
+    if (server is None) or (server == ""):
+        server = 'localhost'
+        database = 'data_viz_demo'
+        user = 'data_viz_user'
+        password = 'data_viz_pw_1!'
 
     db_string = f"postgres://{user}:{password}@{server}:5432/{database}"
     print(f"\t[MSG] Connecting to DB\n\t{db_string}")
+
+    engine = sa.create_engine(db_string)
+
+
+def extract_tables(server, database, user, password):
+    connect_to_db(server, database, user, password)
 
     table_q = """
                 SELECT table_name
@@ -21,7 +26,6 @@ def connect_to_db(server='localhost',
                 ORDER BY table_name;
                 """
     try:
-        engine = sa.create_engine(db_string)
         tables = engine.execute(table_q).fetchall()
         available_tables = [table[0] for table in tables]
         return [available_tables, 'success', None]
@@ -30,3 +34,18 @@ def connect_to_db(server='localhost',
         print(ex.args)
         return [None, 'failure', ex.args]
 
+
+def extract_columns(table):
+    col_q = text("""
+            SELECT *
+            FROM INFORMATION_SCHEMA.COLUMNS
+            WHERE TABLE_NAME = :x;
+            """)
+    try:
+        columns = engine.execute(col_q, x=table).fetchall()
+        available_cols = [col[0] for col in columns]
+        return [available_cols, 'success', None]
+
+    except Exception as ex:
+        print(ex.args)
+        return [None, 'failure', ex.args]
